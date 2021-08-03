90ecef28918f6721cf5a82dddee359d130198d93
Thiago Santiago <thiagosantiagoss20@gmail.com>
2021-08-03 16:30:04 -0300

    Feat: Componente de select criado

---
+++ src/components/select/Select.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export class Select extends React.Component {
+  render() {
+    const { options, event } = this.props;
+
+    const selectMap = options.map((option) => (
+      <option key={option} value={option}>
+        {option}
+      </option>
+    ));
+    return (
+      <select onChange={event} className="selctElement">
+        <option disabled defaultValue>
+          Escolha:
+        </option>
+        {selectMap}
+      </select>
+    );
+  }
+}
+
+export default Select;
