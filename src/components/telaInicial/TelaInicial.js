92ab9af2ee3702d664f26c23edb9fa7d8cf68716
08-06Maisa <may.b.faustino@hotmail.com>
2021-08-03 17:10:22 -0300

    Fiz o header

--- src/components/telaInicial/TelaInicial.js
+++ src/components/telaInicial/TelaInicial.js
@@ -5,20 +5,25 @@ const HeaderContainer = styled.header`
 display: flex;
 justify-content: space-between;
 background-color: orange;
+height: 50px;
+align-items: center;
+padding: 0 1rem;
+nav button:nth-child(1){
+    margin: 0 3px 0 0;
 
+}
 `
 
 export default class TelaInicial extends React.Component{
     render(){
         return(
-            <div>
-                <HeaderContainer>
-                    <header>LabeNinjas</header>
+            <HeaderContainer>
+                <h1>LabeNinjas</h1>
+                <nav>
                     <button>Home</button>
                     <button>Carrinho</button>
-                </HeaderContainer>
-
-            </div>
+                </nav>
+            </HeaderContainer>
         )
     }
 }
